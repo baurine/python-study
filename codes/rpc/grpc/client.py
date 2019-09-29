b989ef81fbdfa90947b69a4e037e33f075043e90
baurine <2008.hbl@gmail.com>
2019-09-29 18:42:22 +0800

    grpc example

---
+++ codes/rpc/grpc/client.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+# client
+
+import grpc
+import pi_pb2
+import pi_pb2_grpc
+
+
+def main():
+    channel = grpc.insecure_channel('localhost:8080')
+    client = pi_pb2_grpc.PiCalculatorStub(channel)
+    for i in range(1, 1000):
+        print "pi(%d) = " % i, client.Calc(pi_pb2.PiRequest(n=i)).value
+
+
+if __name__ == '__main__':
+    main()
